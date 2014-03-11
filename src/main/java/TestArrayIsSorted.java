5ec39257d4b1ed6d0a31bcd0e8fa2f8bf6614af8
foneox <foneox@gmail.com>
2014-03-11 21:33:55 +0200

    testng and pom changed

--- src/main/java/TestArrayIsSorted.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Sergii on 3/11/14.
- */
-public class TestArrayIsSorted {
-}
---
+++ src/test/java/TestArrayIsSorted.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Sergii on 3/11/14.
+ * [home task]
+ Тестирование сортировки массива по возрастанию. Предавать массив для тестирования через data provider, или parameters.
+ */
+        import org.testng.Assert;
+        import org.testng.annotations.DataProvider;
+        import org.testng.annotations.Parameters;
+        import org.testng.annotations.Test;
+
+public class TestArrayIsSorted {
+    @DataProvider(name = "test1")
+    public Object[][] provideData(){
+        return new Object[][]{
+                {10, 10},
+                {100,100},
+                {200,200}
+        };
+    }
+    @Test(dataProvider = "test1")
+    public void testingTest2(int value1, int value2){
+//System.out.println("New TEST!!!! "+ param1 +param2);
+        Assert.assertEquals(value1, value2);
+    }
+}
